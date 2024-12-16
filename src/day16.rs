7b9bfd679c5c58e7d0f461d81bb4f0a7a0986663
LOU Xun <aquarhead@gmail.com>
2024-12-16 20:11:04 +0000

    Day 16 p2

--- src/day16.rs
+++ src/day16.rs
@@ -1,4 +1,4 @@
-use pathfinding::prelude::dijkstra;
+use pathfinding::directed::dijkstra;
 use std::collections::HashSet;
 
 type Pos = (i32, i32);
@@ -31,6 +31,17 @@ impl Reindeer {
     ret
   }
 
+  fn backward(&self) -> Self {
+    let mut ret = self.clone();
+    match self.dir {
+      North => ret.pos.0 += 1,
+      South => ret.pos.0 -= 1,
+      East => ret.pos.1 -= 1,
+      West => ret.pos.1 += 1,
+    }
+    ret
+  }
+
   fn turn(&self) -> (Self, Self) {
     let mut a = self.clone();
     let mut b = self.clone();
@@ -48,7 +59,7 @@ impl Reindeer {
   }
 }
 
-pub fn solve(input: &str) -> (i32, i32) {
+pub fn solve(input: &str) -> (i32, usize) {
   let mut tiles = HashSet::new();
   let mut start = (0, 0);
   let mut end = (0, 0);
@@ -69,22 +80,60 @@ pub fn solve(input: &str) -> (i32, i32) {
 
   let start = Reindeer { pos: start, dir: East };
 
-  let (_, p1) = dijkstra(
-    &start,
-    |r| {
-      let (a, b) = r.turn();
-      let mut succ = vec![(a, 1000), (b, 1000)];
-      let rf = r.forward();
-      if tiles.contains(&rf.pos) {
-        succ.push((rf, 1));
-      }
-      succ
-    },
-    |r| r.pos == end,
-  )
-  .unwrap();
+  let da = dijkstra::dijkstra_all(&start, |r| {
+    let (a, b) = r.turn();
+    let mut succ = vec![(a, 1000), (b, 1000)];
+    let rf = r.forward();
+    if tiles.contains(&rf.pos) {
+      succ.push((rf, 1));
+    }
+    succ
+  });
 
-  (p1, 0)
+  let p1 = da
+    .iter()
+    .filter_map(|(r, (_, d))| (r.pos == end).then(|| d))
+    .min()
+    .unwrap();
+
+  let mut p2 = HashSet::new();
+  let mut search: HashSet<_> = da
+    .iter()
+    .filter_map(|(r, (_, d))| (r.pos == end && d == p1).then(|| (*r, *d)))
+    .collect();
+
+  while !search.is_empty() {
+    p2.extend(search.clone());
+
+    let mut new_search = HashSet::new();
+    for s in search {
+      let br = s.0.backward();
+      if let Some((_, d)) = da.get(&br) {
+        if *d == s.1 - 1 {
+          new_search.insert((br, *d));
+        }
+      }
+
+      let (a, b) = s.0.turn();
+      if let Some((_, d)) = da.get(&a) {
+        if *d == s.1 - 1000 {
+          new_search.insert((a, *d));
+        }
+      }
+
+      if let Some((_, d)) = da.get(&b) {
+        if *d == s.1 - 1000 {
+          new_search.insert((b, *d));
+        }
+      }
+    }
+
+    search = new_search;
+  }
+
+
+
+  (*p1, p2.into_iter().map(|(r, _)| r.pos).collect::<HashSet<_>>().len())
 }
 
 #[cfg(test)]
